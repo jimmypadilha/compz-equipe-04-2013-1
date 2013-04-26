610e49880021f989984137e707fbe7c6942b54f8
ansoli <ansoli@gmail.com>
2013-04-26 18:44:59 -0400

    Criada função para inserir número na tabela e imprimir os valores da tabela.

--- src/questao_3.c
+++ src/questao_3.c
@@ -13,16 +13,35 @@
 
 #define MAX 5
 
+void inserir_numero(int *vetor, int *tabela_hash){
+	int i, valor_hash = 0;
+	for (i = 0; i < MAX; i++) {
+		valor_hash = vetor[i] % 7;
+		tabela_hash[valor_hash] = vetor[i];
+	}
+}
+
+void imprimir_tabela(int *vetor){
+	int i;
+	printf("Números gerados:\n[");
+	for (i = 0; i < MAX; i++){
+		printf("%d ", vetor[i]);
+	}
+	printf("]\n");
+}
+
 int main(void) {
 
 	int i;
-	//int vetor[MAX];
 	int chave_procurada = 12;
 	int tabela_hash[MAX];
-	int valor_hash;
+	int valor_hash = 0;
 
 	/* Vetor de números */
 	int vetor[MAX] = {17, 42, 9, 33, 12};
+
+	inserir_numero(vetor, tabela_hash);
+	imprimir_tabela(tabela_hash);
 
 	/* Gera número aleatório */
 	/*
@@ -31,47 +50,16 @@ int main(void) {
 	}
 	*/
 
-	/* Imprime números gerados */
-	/*
-	printf("Números gerados:\n[");
-	for (i = 0; i < MAX; i++){
-		printf("%d ", vetor[i]);
-	}
-	printf("]\n");
-	*/
-
-	/* Cálculo do valor hash */
-	/*int valor_hash = (580625685 % 701) + 1;
-
-	printf("Valor hash: %d", valor_hash);
-	printf("\nChave do elemento do vetor: %d", vetor[valor_hash]);
-	*/
-
-	/* Inserindo um novo registro */
-	for (i = 0; i < MAX; i++) {
-		valor_hash = (vetor[i] % 701) + 1;
-		//printf("%d", valor_hash); 18 43 10 34 13
-		//printf("\n");
-		tabela_hash[valor_hash] = vetor[i];
-	}
-
-	/* Imprime números gerados */
-	printf("Tabela hash:\n[");
-	for (i = 0; i < 50; i++){
-		if (tabela_hash[i] != "")
-			printf("%d ", tabela_hash[i]);
-	}
-	printf("]\n");
-
 	/* Procurando uma chave */
-	valor_hash = (chave_procurada % 701) + 1;
+	/*
+	valor_hash = (chave_procurada % 7) + 1;
 	for (i = valor_hash;  i < MAX; i++) {
 		if (chave_procurada == tabela_hash[i])
 			printf("\nNúmero encontrado");
 		else
 			printf("\nNúmero não encontrado");
 	}
-
+	*/
 
 	return EXIT_SUCCESS;
 }
