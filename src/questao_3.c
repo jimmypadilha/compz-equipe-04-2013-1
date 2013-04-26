7bf4c13ef85226a6e2a69b23530fb7dea10b7445
ansoli <ansoli@gmail.com>
2013-04-26 00:23:19 -0400

    Inserindo números na tabela hash.

--- src/questao_3.c
+++ src/questao_3.c
@@ -17,6 +17,9 @@ int main(void) {
 
 	int i;
 	//int vetor[MAX];
+	int chave_procurada = 12;
+	int tabela_hash[MAX];
+	int valor_hash;
 
 	/* Tabela hash */
 	int vetor[MAX] = {17, 42, 9, 33, 12};
@@ -37,13 +40,33 @@ int main(void) {
 	printf("]\n");
 	*/
 
-	/* Cálculo da chave */
-	int valor = 580625685 % 701;
-	int chave = valor + 1;
+	/* Cálculo do valor hash */
+	/*int valor_hash = (580625685 % 701) + 1;
 
+	printf("Valor hash: %d", valor_hash);
+	printf("\nChave do elemento do vetor: %d", vetor[valor_hash]);
+	*/
 
-	printf("Chave: %d", chave);
-	printf("\nValor da chave: %d", vetor[chave]);
+	/* Inserindo um novo registro */
+	for (i = 0; i < MAX; i++) {
+		valor_hash = (vetor[i] % 701) + 1;
+		tabela_hash[valor_hash] = vetor[i];
+	}
+
+	/* Imprime números gerados */
+
+	printf("Tabela hash:\n[");
+	for (i = 0; i < MAX; i++){
+		printf("%d ", tabela_hash[i]);
+	}
+	printf("]\n");
+
+	/* Procurando uma chave */
+	if (chave_procurada == vetor[valor_hash])
+		printf("\nNúmero encontrado");
+	else
+		printf("\nNúmero não encontrado");
+
 
 	return EXIT_SUCCESS;
 }
