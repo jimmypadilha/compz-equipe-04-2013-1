1d76453646e8c9510c4b7636467558e77ddbc5aa
ansoli <ansoli@gmail.com>
2013-04-26 18:54:04 -0400

    Inserida função procurar valor.

--- src/questao_3.c
+++ src/questao_3.c
@@ -30,34 +30,37 @@ void imprimir_tabela(int *vetor){
 	printf("]\n");
 }
 
+void procurar_valor(int chave_procurada, int *tabela_hash){
+	int i;
+	int posicao = posicao = chave_procurada % 7;
+
+	for (i = posicao; i < MAX; i++){
+		if (chave_procurada == tabela_hash[i])
+			printf("\nNúmero encontrado");
+		else
+			printf("\nNúmero não encontrado");
+	}
+}
+
 int main(void) {
 
 	int i;
 	int chave_procurada = 12;
 	int tabela_hash[MAX];
 	int valor_hash = 0;
+	int posicao = 0;
 
 	/* Vetor de números */
 	int vetor[MAX] = {17, 42, 9, 33, 12};
 
 	inserir_numero(vetor, tabela_hash);
 	imprimir_tabela(tabela_hash);
+	procurar_valor(chave_procurada, tabela_hash);
 
 	/* Gera número aleatório */
 	/*
 	for (i = 0; i < MAX; i++) {
 		vetor[i] = rand();
-	}
-	*/
-
-	/* Procurando uma chave */
-	/*
-	valor_hash = (chave_procurada % 7) + 1;
-	for (i = valor_hash;  i < MAX; i++) {
-		if (chave_procurada == tabela_hash[i])
-			printf("\nNúmero encontrado");
-		else
-			printf("\nNúmero não encontrado");
 	}
 	*/
 
