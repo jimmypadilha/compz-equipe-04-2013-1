c1040a66399c1548cda6260aab4756e1c241524b
ansoli <ansoli@gmail.com>
2013-04-27 02:50:14 -0400

    Mudança do tamanho do vetor.

--- src/questao_4.c
+++ src/questao_4.c
@@ -11,8 +11,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 5
+#define MAX 100000
 #define M 7
+
+void gerar_numero(int *vetor){
+	int i;
+	for (i = 0; i < MAX; i++) {
+		vetor[i] = rand() % 1000000;
+	}
+}
 
 void iniciar_tabela(int *tabela_hash){
 	int i;
@@ -28,7 +35,13 @@ void inserir_numero(int *vetor, int *tabela_hash){
 		if (tabela_hash[valor_hash] == NULL)
 			tabela_hash[valor_hash] = vetor[i];
 		else{
-			for (j = 0; j < MAX; j++){
+			for (j = valor_hash + 1; j < MAX; j++){
+				if (tabela_hash[j] == NULL){
+					tabela_hash[j] = vetor[i];
+					break;
+				}
+			}
+			for (j = 0; j < valor_hash; j++){
 				if (tabela_hash[j] == NULL){
 					tabela_hash[j] = vetor[i];
 					break;
@@ -54,41 +67,37 @@ void procurar_valor(int chave_procurada, int *tabela_hash){
 	if (chave_procurada == tabela_hash[posicao]){
 		printf("\nNúmero encontrado");
 		printf("\nPosica %d", posicao);
-		return;
 	}
 	else{
 		for (i = posicao + 1; i < MAX; i++){
 			if (chave_procurada == tabela_hash[i]){
-				printf("\nNúmero encontrado");
-				printf("\nPosica %d", posicao);
-				return;
+				printf("\nNúmero encontrado\n");
+				break;
 			}
 		}
-		printf("\nNúmero não encontrado");
+		for (i = 0; i < posicao; i++){
+			if (chave_procurada == tabela_hash[i]){
+				printf("\nNúmero encontrado\n");
+				break;
+			}
+		}
+		printf("\nNúmero não encontrado\n");
 	}
 }
 
 int main(void) {
-
-	int i;
-	int chave_procurada = 17;
+	int chave_procurada;
 	int tabela_hash[MAX];
-	int valor_hash = 0;
-	int posicao = 0;
+	int vetor[MAX];
 
-	int vetor[MAX] = {17, 42, 9, 33, 12};
+	printf("Informe o valor a ser pesquisado: ");
+	scanf("%d", &chave_procurada);
 
+	gerar_numero(vetor);
 	iniciar_tabela(tabela_hash);
 	inserir_numero(vetor, tabela_hash);
 	imprimir_tabela(tabela_hash);
 	procurar_valor(chave_procurada, tabela_hash);
-
-	/* Gera número aleatório */
-	/*
-	for (i = 0; i < MAX; i++) {
-		vetor[i] = rand();
-	}
-	*/
 
 	return EXIT_SUCCESS;
 }
