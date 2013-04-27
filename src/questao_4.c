5818bbad559d2c7968f17033c71bee424e845b72
ansoli <ansoli@gmail.com>
2013-04-26 20:41:53 -0400

    Solução para o problema da inserção dos dados da terceira questão acabou sendo o mecanismo do linear probing.

--- src/questao_4.c
+++ src/questao_4.c
@@ -11,8 +11,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX 5
+#define M 7
+
+void iniciar_tabela(int *tabela_hash){
+	int i;
+	for (i = 0; i < MAX; i++) {
+		tabela_hash[i] = NULL;
+	}
+}
+
+void inserir_numero(int *vetor, int *tabela_hash){
+	int i, j, valor_hash = 0;
+	for (i = 0; i < MAX; i++) {
+		valor_hash = vetor[i] % MAX;
+		if (tabela_hash[valor_hash] == NULL)
+			tabela_hash[valor_hash] = vetor[i];
+		else{
+			for (j = 0; j < MAX; j++){
+				if (tabela_hash[j] == NULL){
+					tabela_hash[j] = vetor[i];
+					break;
+				}
+			}
+		}
+	}
+}
+
+void imprimir_tabela(int *vetor){
+	int i;
+	printf("Números gerados:\n[");
+	for (i = 0; i < MAX; i++){
+		printf("%d ", vetor[i]);
+	}
+	printf("]\n");
+}
+
+void procurar_valor(int chave_procurada, int *tabela_hash){
+	int i;
+	int posicao = chave_procurada % MAX;
+
+	if (chave_procurada == tabela_hash[posicao]){
+		printf("\nNúmero encontrado");
+		printf("\nPosica %d", posicao);
+		return;
+	}
+	else{
+		for (i = posicao + 1; i < MAX; i++){
+			if (chave_procurada == tabela_hash[i]){
+				printf("\nNúmero encontrado");
+				printf("\nPosica %d", posicao);
+				return;
+			}
+		}
+		printf("\nNúmero não encontrado");
+	}
+}
+
 int main(void) {
 
+	int i;
+	int chave_procurada = 17;
+	int tabela_hash[MAX];
+	int valor_hash = 0;
+	int posicao = 0;
+
+	int vetor[MAX] = {17, 42, 9, 33, 12};
+
+	iniciar_tabela(tabela_hash);
+	inserir_numero(vetor, tabela_hash);
+	imprimir_tabela(tabela_hash);
+	procurar_valor(chave_procurada, tabela_hash);
+
+	/* Gera número aleatório */
+	/*
+	for (i = 0; i < MAX; i++) {
+		vetor[i] = rand();
+	}
+	*/
 
 	return EXIT_SUCCESS;
 }
